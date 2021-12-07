52931477e33dcaa0f2853f107bb407ac7c1951fb
Nick23t <94141672+Nick23t@users.noreply.github.com>
2021-12-07 13:30:49 -0500

    Delete homework.sh

--- homework.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -n1 -p "Do you want to display your assignments? [y,n]" class 
-case $assignment in  
-  y|Y) cat homework ;; 
-  n|N) echo User does not want to display assignments  ;; 
-  *) echo Please enter a y or n ;; 
-esac
