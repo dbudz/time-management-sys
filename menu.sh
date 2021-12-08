d14a69f54a5d4851a46b667516d3de0523da0f3d
dbudz <dbudzitowski14@gmail.com>
2021-12-07 22:31:44 -0500

    completed menu and changed filename to menu.sh, changed record files to delimited files using semicolons

---
+++ menu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+tput clear
+bold=$(tput smso)
+offbold=$(tput rmso)
+echo $bold
+PS3="Enter what you would like to view: "
+ 
+select menu_item in "Classes" "Assignments" "Incomplete Assignments" "Add an Assignment" "Update an Assignment" "Quit"
+do
+	echo $menu_item
+	case $menu_item in
+		"Classes")
+			echo "1"
+			;;
+		"Assignments")
+			echo "2"
+			;;
+		"Incomplete Assignments")
+			echo "3"
+			;;		
+		"Add an Assignment")
+			echo "4"
+			;;		
+		"Update an Assignment")
+			echo "5"
+			;;
+		"Quit")
+			exit
+			;;
+		*)		
+			echo "Error: Please try again (select 1..6)!"
+			;;		
+	esac
+done
+echo $offbold
