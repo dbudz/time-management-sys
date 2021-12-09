6de2eb8006c9266acb1ac635bfed01b1349ec168
thing102 <94816131+thing102@users.noreply.github.com>
2021-12-08 20:47:48 -0500

    Update update_assignment.sh

--- update_assignment.sh
+++ update_assignment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 update_assignment () {
-	read -n1 -p "Would you like to update an assignment? [y,n]" class 
+	echo "Would you like to update an assignment? [y,n]" class 
 case $class in  
   y|Y) cat assignment ;; 
   n|N) echo No changes made  ;; 
