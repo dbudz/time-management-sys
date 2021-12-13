35f4044cdd428b709e0bac363b8b05fd58b0d380
Nick23t <94141672+Nick23t@users.noreply.github.com>
2021-12-13 16:07:21 -0500

    Update update_assignment.sh

--- update_assignment.sh
+++ update_assignment.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
-update_assignment () {
-	echo "Would you like to update an assignment? [y,n]" class 
-case $class in  
-  y|Y) cat assignment ;; 
-  n|N) echo No changes made  ;; 
-  *) echo Please enter a y or n ;; 
+!/bin/bash
+read -p "What status would you like to change?" status
+case $status in
+  Open)
+    sed -i -e 's/"Open"/"Completed"/g' assignments
+    echo You have updated the status of your open assignments to completed
+    ;;
+  Completed)
+    sed -i -e 's/"Completed"/"Open"/g' assignments
+    echo You have updated the status of your completed assignments to open
+    ;;
+  *) echo Please enter the status you would like to change
 esac
-		}
+
+
